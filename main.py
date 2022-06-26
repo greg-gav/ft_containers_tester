3778de2bf713cee427a37e1aea2a9938d6844fa9
bteak <bteak@student.21-school.ru>
2022-06-26 15:26:38 +0300

    funcs ftw

--- main.py
+++ main.py
@@ -9,28 +9,35 @@ TEMP_FOLDER = "./tmp"
 FLAGS = "-I./tests -Wall -Werror -Wextra -std=c++98"
 
 def main():
-    path = "./"
-    
-    if len(sys.argv) == 2:
-        path = sys.argv[1]
+    path = get_path_or_exit()
 
-    #assemble vector tests if arg == vec
+    #assemble tests --> vector
+    with open(f"{TEST_FOLDER}/{VEC_TESTS_H}", "r") as header_file:
+        h_lines = header_file.readlines()
+
+    includes = []
+    for line in h_lines:
+        if line.startswith("#include"):
+            includes.append(line)
+            
+    while(loc := parse_header(h_lines) > 0):
+        break
+
     header_file =  open(f"{TEST_FOLDER}/{VEC_TESTS_H}", "r")
     cpp_file = open(f"{TEST_FOLDER}/{VEC_TESTS_CPP}", "r")
     out_name = f"{TEMP_FOLDER}/outfile.cpp"
     os.makedirs(os.path.dirname(out_name), exist_ok=True)
     out_file = open(out_name, "w")
-    lines = header_file.readlines()
+    
     add_func = False
     func_name = ""
     for l in lines:
-        if l.startswith("#include"):
-            out_file.write(l)
         if l.startswith("void "):
             out_file.write(l)
             add_func = True
             func_name = re.search(r"^.* ([A-za-z_]+)\(.*$", l).group(1)
             break
+    out_file.write("std::string     test_name;\n")
     if add_func:
         cpplines = cpp_file.readlines()
         for i, l in enumerate(cpplines):
@@ -48,10 +55,19 @@ def process_func(lines, i, outfile):
             cl += 1
         if op == cl:
             break
-    for k in range(i,j):
-        outfile.write
-    
-            
+    for k in range(i,j + 1):
+        outfile.write(lines[k])
+
+#return line number with next prototype
+def parse_header(lines): 
+    return 1
+parse_header.progress = 0
+
+def get_path_or_exit():
+    path = "./"
+    if len(sys.argv) == 2:
+        path = sys.argv[1]
+    return path
 
 if __name__ == "__main__":
     main()
